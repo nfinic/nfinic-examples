eed5029271e0e587680a53481417771762aba930
Eliel <elielmathe@nfinic.com>
2019-12-01 14:10:51 +0200

    Richard a recommande ces changements

--- php-example/index.php
+++ php-example/index.php
@@ -7,4 +7,4 @@
 	
 	
 	//Ce bouton est a placer sur votre page de connexion
-	echo '<a href="'.$lien.'" style="text-decoration:none;background:white;color:white;font-size:20px;font-family:;background:#205560;border:1px solid #205560;padding:10px;margin:10px;margin-top:20px;margin-bottom:20px;border-radius:5px;"><img style="height:30px;position:relative;top:7px" src="https://nfinic.com/logo.white.transparent.png"/> | Connexion avec nfinic</a>';
+	echo '<a href="'.$lien.'" style="text-decoration:none;background:white;color:white;font-size:16px;font-family:;background:#205560;border:1px solid #205560;padding:10px;margin:10px;margin-top:20px;margin-bottom:20px;border-radius:5px;"><img style="height:20px;" src="https://nfinic.com/logo.white.transparent.png"/> | Connexion avec nfinic</a>';
