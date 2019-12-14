dcde044b56ed3ca368dc5059261c12c21c7befad
Eliel <elimek2@gmail.com>
2019-12-14 12:03:32 +0200

    New comments

--- php-example-mysql/redirection.php
+++ php-example-mysql/redirection.php
@@ -1,7 +1,7 @@
 <?php
     /**
     /* Add here the link where you redirect the user once connected
-    /* AJouter ici le lien de redirection lorsque l'utilisateur est connecte
+    /* Ajouter ici le lien de redirection lorsque l'utilisateur est connecte
     */
     
     header("location:main.php");
